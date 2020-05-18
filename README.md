40db9b42b3597df00918cc1904f736253330c8b2
Lanmor Yang <lanmor.yang@adgeek.com.tw>
2020-05-18 11:30:32 +0800

    initial

---
+++ README.md
@@ -0,0 +1,52 @@
+# yii2-fluentd
+
+Logging with Fluentd for Yii2.
+
+============================
+
+Installation
+------------
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require "cyntelli/yii2-log-fluentd" "*"
+```
+
+or add
+
+```json
+"cyntelli/yii2-log-fluentd" : "*"
+```
+
+to the `require` section of your application's `composer.json` file.
+
+Usage
+-----
+
+Add Fluentd target to your log component config:
+```php
+<?php
+return [
+    ...
+    'components' => [
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            // 'flushInterval' => 1,
+            'targets' => [
+                'fluentTarget' => [
+                    'class' => 'cyntelli\log\FluentdTarget',
+                    'levels' => ['error', 'warning'], // Log levels
+                    // 'exportInterval' => 1,
+                    'host' => 'host', // Fluentd host
+                    'port' => '24224', // Fluentd port
+                    'options' => [], // Options for Fluentd client
+                    'tag' => 'app' // Tag
+                ]
+            ],
+        ],
+    ],
+    ...
+];
+```
---
+++ src/FluentdTarget.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * @link https://www.cyntelli.com
+ * @copyright Copyright (c) 2020 Cyntelli
+ * @license https://github.com/Cyntelli/yii2-fluentd-target/LICENSE
+ */
+
+namespace cyntelli\log;
+
+use Fluent\Logger\FluentLogger;
+use yii\helpers\ArrayHelper;
+use yii\helpers\VarDumper;
+use yii\log\Logger;
+
+/**
+ * FluentdTarget sends log messages to Fluentd
+ 
+ * @author Lanmor Yang <lanmor.yang@adgeek.com.tw>
+ * @version 0.1.0
+ */
+class FluentdTarget extends \yii\log\Target
+{
+    /**
+     * @var array $levels Logger::LEVEL_*
+     */
+    public $levels;
+    /**
+     * @var string Fluentd host
+     */
+    public $host = 'localhost';
+    /**
+     * @var int Fluentd port
+     */
+    public $port = 24224;
+    /**
+     * @var array Options for Fluentd client
+     */
+    public $options = [];
+
+    /**
+     * @var string Tag 
+     */
+    public $tag = 'app';
+
+    private $_logger;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->_logger = FluentLogger::open($this->host, $this->port, $this->options);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function export()
+    {
+        foreach ($this->messages as $message) {
+            list($text, $level, $category, $timestamp) = $message;
+            $level = Logger::getLevelName($level);
+
+            if (!in_array($level, $this->levels)) continue;
+            $shortMsg = '';
+            $fullMsg = '';
+            $line = '';
+            $file = '';
+            // For string log message set only shortMessage
+            if (is_string($text)) {
+                $shortMsg = $text;
+            } elseif ($text instanceof \Exception) {
+                $shortMsg = ('Exception ' . get_class($text) . ': ' . $text->getMessage());
+                $fullMsg = ((string) $text);
+                $line = $text->getLine();
+                $file = $text->getFile();
+            } else {
+                $short = ArrayHelper::remove($text, 'short');
+                $full = ArrayHelper::remove($text, 'full');
+
+                if ($short !== null) {
+                    $shortMsg = $short;
+                    // All remaining message is fullMessage by default
+                    $fullMsg = VarDumper::dumpAsString($text);
+                } else {
+                    // Will use log message as shortMessage by default (no need to add fullMessage in this case)
+                    $shortMsg = VarDumper::dumpAsString($text);
+                }
+                // If 'full' is set will use it as fullMessage (note that all other stuff in log message will not be logged, except 'short' and 'add')
+                if ($full !== null) {
+                    $fullMsg = (VarDumper::dumpAsString($full));
+                }
+            }
+       
+            if (isset($message[4]) && is_array($message[4])) {
+                $traces = [];
+                foreach ($message[4] as $index => $trace) {
+                    $traces[] = "{$trace['file']}:{$trace['line']}";
+                }
+                $trace = implode("\n", $traces);
+            }
+            $finalMsg = [
+                'message' => $shortMsg,
+                'message_full' => $fullMsg,
+                'file' => $file,
+                'trace' => $trace,
+                'level' => $level,
+                'category' => $category,
+                'line' => $line
+            ];
+            $this->_logger->post($this->tag, $finalMsg);
+        }
+    }
+}
